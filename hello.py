8ff004dd6cf356a9fee65002c9741bb508bd97e9
liyingxiu <yingxiuim@gmail.com>
2021-03-17 15:47:20 +0800

    sql

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask import request
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bd = SQLAlchemy(app)
