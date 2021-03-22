81f9ed2c55be579ba6273b6ee4eae18182212326
liyingxiu <yingxiuim@gmail.com>
2021-03-22 17:30:44 +0800

    闪现消息

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -13,11 +13,15 @@ class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-
+# 如果新提交的名字和之前存储的名字不同，则会调用flash函数
+# 如果只调用flash函数并不能显示出来，需要使用模版进行渲染
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("looks like you have changed your name!")
         session["name"] = form.name.data
         return redirect(url_for("index"))
     return render_template("index.html", form=form, name=session.get("name"))
