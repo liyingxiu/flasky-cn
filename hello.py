928e5be6398bd749d5dd9a7a61bd1a63ce1a1a47
liyingxiu <yingxiuim@gmail.com>
2021-03-22 15:55:03 +0800

    时间设定3e

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", current_time=datetime.time())
 
 @app.route("/user/<name>")
 def user(name):
